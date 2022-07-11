29bf292fbcd82c852d6bb555bd84b0e28648196a
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-07-11 13:40:07 -0300

    feat: modelando o banco de dados

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const dbConfig = require('../config/db.config');
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: 0,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require('../models/user.model')(sequelize, Sequelize);
+
+
+module.exports = db
+    
---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => { 
+    
+    return User = sequelize.define('user', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return  User;
+}
